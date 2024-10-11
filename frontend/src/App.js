bd9090d8576230b90934f57ea3a9037eaeda69c7
VHarsha8 <harshavardhan6304@gmail.com>
2024-10-11 21:40:48 +0530

    Redesign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ExampleComponent from './MuiComponent';
-import About from './About';
 import TiltCard from './TiltCard';
 
 function App() {
